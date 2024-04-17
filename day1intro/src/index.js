074b0baccd8dd214e1bef39cbbeeda5feb96da13
sapna833 <127634052+sapna833@users.noreply.github.com>
2024-04-17 16:06:58 +0530

    Add files via upload

---
+++ day1intro/src/index.js
@@ -0,0 +1,8 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+ReactDOM.render(<h1>Hello everyone </h1>, document.getElementById("root"));
+
+// second way to acheive it
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "hello everyone";
+// document.getElementById("root").appendChild(h1);
